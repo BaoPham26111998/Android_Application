942c77e776b49b7c63874e4a1256ddbc02ce647e
Bao Pham <s3695363@rmit.edu.vn>
2022-01-14 16:33:27 +0700

    merge video

---
+++ app/src/main/java/com/example/android_application/activities/MainVideo.java
@@ -0,0 +1,14 @@
+package com.example.android_application.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_video);
+    }
+}
